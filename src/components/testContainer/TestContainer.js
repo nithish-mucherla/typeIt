06a932fc53252e6b0d3a591ba0dff5f5fc92ee89
nithish-mucherla <mucherlanithu@gmail.com>
2021-05-07 18:34:21 +0530

    reduce fetch para count to 1

--- src/components/testContainer/TestContainer.js
+++ src/components/testContainer/TestContainer.js
@@ -11,7 +11,7 @@ const TestContainer = () => {
   const [timeLeft, setTimeLeft] = useState(60);
   //http://metaphorpsum.com/paragraphs/2/10
   useEffect(() => {
-    fetch("https://mashape-community-skate-ipsum.p.rapidapi.com/3/0/JSON", {
+    fetch("https://mashape-community-skate-ipsum.p.rapidapi.com/1/0/JSON", {
       method: "GET",
       headers: {
         "x-rapidapi-key": "3d15874e0amshf1bdb48a55c5499p100e08jsn9ee781838fa3",
@@ -19,7 +19,7 @@ const TestContainer = () => {
       },
     })
       .then((res) => res.text())
-      .then((para) => setTestPara(para))
+      .then((para) => setTestPara(para.slice(2, para.length - 2)))
       .catch((err) => {
         const para = `An airbus is a leg from the right perspective. Before pantries, harmonies were only baritones. Few can name a serene mistake that isn't a stockinged sword. Casebook nodes show us how pinks can be houses. This is not to discredit the idea that a key is a distrait interest. A step-grandfather of the carp is assumed to be an algal sunflower. A sneaking pine is a swing of the mind. Some ocher coins are thought of simply as slashes. Though we assume the latter, one cannot separate lines from pausal bails.`;
         setTestPara(para);
